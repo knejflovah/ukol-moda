2abf5d7bba8e927033a3f1a7c255ffa312fa6c49
knejflovah <knejflovah@gmail.com>
2022-03-11 16:26:52 +0100

    mujprvniukol

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Dresses from './components/Dresses';
+import Shoes from './components/Shoes';
+import Accessories from './components/Accessories';
+
+
 
 const App = () => (
   <>
-    <p>Tady asi něco bude</p>
+
+    <div className="container">
+      <Header title="Móda" />
+    
+      <Dresses />
+      <Shoes />
+      <Accessories />
+      
+      <Footer year=" 2022" author=" Czechitas" />
+    </div>
+
+
   </>
 );
 
